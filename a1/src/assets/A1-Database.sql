7c792c82aa34ba420ba0a2056cd8171d5e2ada5f
Rexisk <darrenfoo7@gmail.com>
2024-09-27 10:12:55 +0800

    SQL + CSV update

---
+++ a1/src/assets/A1-Database.sql
@@ -0,0 +1,106 @@
+CREATE DATABASE a1_database;
+USE a1_database;
+
+CREATE TABLE Role (
+    Role INT PRIMARY KEY AUTO_INCREMENT,
+    Role_Name VARCHAR(50) NOT NULL UNIQUE,
+    Role_Description TEXT
+);
+
+
+CREATE TABLE Employee (
+    Staff_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Staff_FName VARCHAR(50) NOT NULL,
+    Staff_LName VARCHAR(50) NOT NULL,
+    Dept VARCHAR(50) NOT NULL,
+    Position VARCHAR(50) NOT NULL,
+    Country VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL UNIQUE,
+    Reporting_Manager INT,
+    Role INT NOT NULL,
+    FOREIGN KEY (Reporting_Manager) REFERENCES Employee(Staff_ID),
+    FOREIGN KEY (Role) REFERENCES Role(Role)
+);
+
+CREATE TABLE User (
+    Staff_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Email VARCHAR(50) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    FOREIGN KEY (Staff_ID) REFERENCES Employee(Staff_ID),
+    FOREIGN KEY (Email) REFERENCES Employee(Email)
+);
+
+
+CREATE TABLE Work_Request (
+    Request_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Staff_ID INT NOT NULL,
+    Request_Type VARCHAR(20) NOT NULL,
+    Request_Date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Reason TEXT,
+    Status VARCHAR(20) NOT NULL DEFAULT 'Pending',
+    Approval_Manager_ID INT,
+    Decision_Date DATETIME,
+    Comments TEXT,
+    FOREIGN KEY (Staff_ID) REFERENCES Employee(Staff_ID),
+    FOREIGN KEY (Approval_Manager_ID) REFERENCES Employee(Staff_ID)
+);
+
+CREATE TABLE Schedule (
+    Schedule_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Staff_ID INT NOT NULL,
+    Date DATE NOT NULL,
+    Location VARCHAR(50) NOT NULL,
+    Request_ID INT,
+    Created_By INT NOT NULL,
+    Created_Date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (Staff_ID) REFERENCES Employee(Staff_ID),
+    FOREIGN KEY (Request_ID) REFERENCES Work_Request(Request_ID),
+    FOREIGN KEY (Created_By) REFERENCES Employee(Staff_ID)
+);
+
+CREATE TABLE Audit (
+    Log_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Request_ID INT NOT NULL,
+    Manager_ID INT NOT NULL,
+    Action_Taken VARCHAR(20) NOT NULL,
+    Timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Comments TEXT,
+    FOREIGN KEY (Request_ID) REFERENCES Work_Request(Request_ID),
+    FOREIGN KEY (Manager_ID) REFERENCES Employee(Staff_ID)
+);
+
+INSERT INTO Role (Role, Role_Name, Role_Description)
+VALUES (1, 'HR', NULL);
+
+INSERT INTO Role (Role, Role_Name, Role_Description)
+VALUES (2, 'Staff', NULL);
+
+INSERT INTO Role (Role, Role_Name, Role_Description)
+VALUES (3, 'Manager', NULL);
+
+ALTER TABLE Employee MODIFY Reporting_Manager INT NULL;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+# Change this path to the csv file path
+LOAD DATA INFILE '/Users/darren/Desktop/SQL Stuff/employeenew.csv'
+INTO TABLE Employee
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n' 
+(Staff_ID, Staff_FName, Staff_LName, Dept, Position, Country, Email, @Reporting_Manager, Role)
+SET Reporting_Manager = IF(@Reporting_Manager = '', NULL, @Reporting_Manager);
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+UPDATE Employee SET Reporting_Manager = Staff_ID WHERE Staff_ID = 130002;
+
+INSERT INTO User (Staff_ID, Email, Password)
+VALUES (130002, 'jack.sim@allinone.com.sg', 'Password123');
+
+INSERT INTO User (Staff_ID, Email, Password)
+VALUES (140002, 'Susan.Goh@allinone.com.sg', 'Password123');
+
+INSERT INTO User (Staff_ID, Email, Password)
+VALUES (140894, 'Rahim.Khalid@allinone.com.sg', 'Password123');
+
+
---
+++ a1/src/pages/calendar.tsx
@@ -0,0 +1,13 @@
+const staffcalendar = () => {
+    return (
+    <>
+    <h1>Calendar</h1>
+    <img src="src/assets/naloong.jpg">
+    </img>
+    </>
+    
+    
+    );
+};
+
+export default staffcalendar;
